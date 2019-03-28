adda6db36e33f569066971e7e476d7f65afba479
Teneala Spencer <teneala.spencer@usm.edu>
2019-03-28 11:58:36 -0500

    Create loops_and_if_else.sql

---
+++ loops_and_if_else.sql
@@ -0,0 +1,14 @@
+DECLARE 
+
+  i number (0)
+  j number (2)
+  
+ BEGIN 
+    FOR i IN 1..4 LOOP
+      FOR j IN 1..4 LOOP 
+        i = i + j
+        IF i = 8 THEN 
+          dbms_output.put_line(' i is equal to: ' || i)
+      END loop inner_loop
+    END loop outer_loop
+ END;
