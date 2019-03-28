46eff113a27ec1faa94ba558b760ba560b73536e
Teneala Spencer <teneala.spencer@usm.edu>
2019-03-28 12:13:44 -0500

    Update loops_and_if_else.sql

--- loops_and_if_else.sql
+++ loops_and_if_else.sql
@@ -6,7 +6,7 @@ DECLARE
  BEGIN 
     FOR i IN 1..4 LOOP
       FOR j IN 1..4 LOOP 
-        i = i + j
+        i := i + j
         IF i = 8 THEN 
           dbms_output.put_line(' i is equal to: ' || i)
       END loop inner_loop
